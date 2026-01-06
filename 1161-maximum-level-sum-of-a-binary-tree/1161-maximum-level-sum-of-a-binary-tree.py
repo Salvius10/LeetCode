70777670f545055d4a9d0aff405183034b718ac9
Salvius10 <132757749+Salvius10@users.noreply.github.com>
2026-01-06 10:45:05 +0530

    Time: 46 ms (9.24%), Space: 23.2 MB (13.07%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -8,23 +8,25 @@ class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        max_sum=float('-inf')
+        max_val=float('-inf')
         stack=[root]
-        max_level=1
         level=1
+        best_level=1
         while stack:
             level_size=len(stack)
             sum_val=0
-            for i in range(level_size):
+            
+            for _ in range(level_size):
                 current=stack.pop(0)
-                sum_val=sum_val+current.val
+                sum_val+=current.val
                 if current.left:
                     stack.append(current.left)
                 if current.right:
                     stack.append(current.right)
-            if sum_val > max_sum:
-                max_sum = sum_val
-                max_level = level
-            level+=1
-        return max_level
             
+            if sum_val>max_val:
+                max_val=sum_val
+                best_level=level
+            level+=1
+            
+        return best_level
